c084c0a845285e9847f06f0a62b4ae827a69b6b0
Emanuele Pardini <66436200+EmanuelePardini@users.noreply.github.com>
2023-10-20 17:44:11 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,22 +17,22 @@ Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home');
 
 
 //Orders
-Route::get('/orders', 'App\Http\Controllers\OrderController@index')->name('orders.index');
-Route::get('/orders/create', 'App\Http\Controllers\OrderController@create')->name('orders.create');
-Route::post('/orders', 'App\Http\Controllers\OrderController@store')->name('orders.store');
-Route::get('/orders/{id}', 'App\Http\Controllers\OrderController@show')->name('orders.show');
-Route::get('/orders/{id}/edit', 'App\Http\Controllers\OrderController@edit')->name('orders.edit');
-Route::put('/orders/{id}', 'App\Http\Controllers\OrderController@update')->name('orders.update');
-Route::delete('/orders/{id}', 'App\Http\Controllers\OrderController@destroy')->name('orders.destroy');
-Route::get('/orders/byDateRange/{startDate}/{endDate}', 'App\Http\Controllers\OrderController@showByDateRange')->name('orders.showByDateRange');
-Route::get('/orders/byProduct/{productId}', 'App\Http\Controllers\OrderController@showByProduct')->name('orders.showByProduct');
+Route::get('/orders', 'OrderController@index')->name('orders.index');
+Route::get('/orders/create', 'OrderController@create')->name('orders.create');
+Route::post('/orders', 'OrderController@store')->name('orders.store');
+Route::get('/orders/{id}', 'OrderController@show')->name('orders.show');
+Route::get('/orders/{id}/edit', 'OrderController@edit')->name('orders.edit');
+Route::put('/orders/{id}', 'OrderController@update')->name('orders.update');
+Route::delete('/orders/{id}', 'OrderController@destroy')->name('orders.destroy');
+Route::get('/orders/byDateRange/{startDate}/{endDate}', 'OrderController@showByDateRange')->name('orders.showByDateRange');
+Route::get('/orders/byProduct/{productId}', 'OrderController@showByProduct')->name('orders.showByProduct');
 
 //Products
-Route::get('/products', 'App\Http\Controllers\ProductController@index')->name('products.index');
-Route::get('/products/create', 'App\Http\Controllers\ProductController@create')->name('products.create');
-Route::post('/products', 'App\Http\Controllers\ProductController@store')->name('products.store');
-Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name('products.show');
-Route::get('/products/{id}/edit', 'App\Http\Controllers\ProductController@edit')->name('products.edit');
-Route::put('/products/{id}', 'App\Http\Controllers\ProductController@update')->name('products.update');
-Route::delete('/products/{id}', 'App\Http\Controllers\ProductController@destroy')->name('products.destroy');
-Route::get('/totalRecycledPlastic', 'App\Http\Controllers\ProductController@totalRecycledPlastic')->name('products.totalRecycledPlastic');
+Route::get('/products', 'ProductController@index')->name('products.index');
+Route::get('/products/create', 'ProductController@create')->name('products.create');
+Route::post('/products', 'ProductController@store')->name('products.store');
+Route::get('/products/{id}', 'ProductController@show')->name('products.show');
+Route::get('/products/{id}/edit', 'ProductController@edit')->name('products.edit');
+Route::put('/products/{id}', 'ProductController@update')->name('products.update');
+Route::delete('/products/{id}', 'ProductController@destroy')->name('products.destroy');
+Route::get('/totalRecycledPlastic', 'ProductController@totalRecycledPlastic')->name('products.totalRecycledPlastic');
